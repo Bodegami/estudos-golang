f06ddf4ebd15b2dcd8b92a29a61a91b459db16eb
Renato <rena.f.oliveira@gmail.com>
2025-04-05 16:47:23 -0300

    [day01] tipos e comentarios em go && interpolacao de strings &&conclusao dia 01

--- main/main.go
+++ main/main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Hello World") //String
+
+	fmt.Println(2) //Numericos
+
+	fmt.Println(2 + 5) // Expressoes
+
+	fmt.Println(false) //Booleanos
+
+	//Para fazer a interpolacao, utilizamos o Printf
+	fmt.Printf("Hello, I'm %v and I'm an Integer\n", 2)
+	fmt.Printf("Hello, I'm %v and I'm an String\n", "texto")
+	fmt.Printf("Hello, I'm %v and I'm an Boolean\n", true)
+
+	fmt.Printf("Hello, I'm %d and I'm an Integer\n", 2)
 }
